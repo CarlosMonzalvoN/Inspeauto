84328f974fc4c3adfbbab21ae5ba74b93230f2a3
CesarVargas <cesarvarcard@gmail.com>
2020-02-13 16:36:13 -0600

    Adding login design

---
+++ AutoInspector/AutoInspector/ViewControllers/LoginViewController.swift
@@ -0,0 +1,34 @@
+//
+//  LoginViewController.swift
+//  AutoInspector
+//
+//  Created by Cesar on 13/02/20.
+//  Copyright © 2020 CesarVargas. All rights reserved.
+//
+
+import UIKit
+
+class LoginViewController: UIViewController {
+
+    @IBOutlet weak var emailTextField: UITextField!
+    
+    @IBOutlet weak var passwordTextField: UITextField!
+    
+    @IBOutlet weak var loginButton: UIButton!
+    
+    @IBOutlet weak var errorLabel: UILabel!
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    @IBAction func loginTapped(_ sender: Any) {
+    }
+    
+    
+    
+
+}
---
+++ AutoInspector/AutoInspector/ViewControllers/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  AutoInspector
+//
+//  Created by Cesar on 13/02/20.
+//  Copyright © 2020 CesarVargas. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
