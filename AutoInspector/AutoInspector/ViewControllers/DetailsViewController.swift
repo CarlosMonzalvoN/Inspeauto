6f20b5a2458d07055ff970d05a268babf306e935
CesarVargas <cesarvarcard@gmail.com>
2020-02-23 21:31:01 -0600

    Adding Pdf Creation with preview and LocalAutentication to send & Save  Pdf

--- AutoInspector/AutoInspector/ViewControllers/DetailsViewController.swift
+++ AutoInspector/AutoInspector/ViewControllers/DetailsViewController.swift
@@ -15,22 +15,45 @@ import PDFKit
 
 
 class DetailsViewController: UIViewController {
-    @IBOutlet weak var generateBtn: UIButton!
     
-    public var documentData: Data?
+
+
+    
+    @IBOutlet weak var nameTextEntry: UITextField!
+    @IBOutlet weak var bodyTextView: UITextView!
+    @IBOutlet weak var modelTextField: UITextField!
+    @IBOutlet weak var imagePreview: UIImageView!
+    @IBOutlet weak var generateBtn: UIButton!
+
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
-      
-
+        Styles.styleTextField(nameTextEntry)
+        Styles.styleTextField(modelTextField)
         
+    }
+//Funcion para compartir
+ func shareAction() {
+
+      guard let title = nameTextEntry.text, let body = bodyTextView.text,
+        let image = imagePreview.image,let model = modelTextField.text
+        else {
+
+          let alert = UIAlertController(title: "All Information Not Provided", message: "You must supply all information to create a flyer.", preferredStyle: .alert)
+          alert.addAction(UIAlertAction(title: "OK", style: .default, handler: nil))
+          present(alert, animated: true, completion: nil)
+          return
+      }
+    let pdfCreator = PDFCreator(title: title, body: body, image: image, model: model)
+      let pdfData = pdfCreator.createPDF()
+      let vc = UIActivityViewController(activityItems: [pdfData], applicationActivities: [])
+      present(vc, animated: true, completion: nil)
     }
     
 
 
     @IBAction func generateBtnTapped(_ sender: Any) {
-        
 
         let contxt: LAContext = LAContext()
         contxt.localizedCancelTitle = "Intenta cancelar"
@@ -39,7 +62,12 @@ class DetailsViewController: UIViewController {
         if contxt.canEvaluatePolicy(.deviceOwnerAuthenticationWithBiometrics, error: nil){
             contxt.evaluatePolicy(LAPolicy.deviceOwnerAuthenticationWithBiometrics, localizedReason: "Para Generar PDF con datos privados del usuario por favor confirme que es el dueño de este dispositivo") { (success, error) in
                 if success{
+                    DispatchQueue.main.asyncAfter(deadline: .now() + 0.6) {
+                        
                     showGoodAlert()
+                        
+                    }
+                     
                     
                 }else{
                     showBadAlert()
@@ -47,10 +75,14 @@ class DetailsViewController: UIViewController {
             }
         }
         
+        
         func showGoodAlert(){
             let alert = UIAlertController(title: "Autenticacion", message: "La autenticacion se realizo con exito", preferredStyle: .alert)
-            let ok  = UIAlertAction(title: "Aceptar", style: .default, handler: nil)
-            alert.addAction(ok)
+           
+            alert.addAction(UIAlertAction(title: "Aceptar", style: .default, handler: { [weak alert, weak self] (_) in
+                self?.shareAction()
+            }))
+            
             DispatchQueue.main.async {
                 //Presentamos la alerta a
                 self.present(alert, animated: true)
@@ -71,32 +103,71 @@ class DetailsViewController: UIViewController {
 
         }
         
-        
-        func createPdf() -> Data{
-            let pdfMetaData = [
-                kCGPDFContextCreator: "Flyer Builder",
-                kCGPDFContextAuthor: "Cesar Vargas AutoInspector"
-            ]
-            let format = UIGraphicsPDFRendererFormat()
-            format.documentInfo = pdfMetaData as [String: Any]
-            
-            let pageWidth = 8.5 * 72.0
-            let pageHeigth = 11 * 72.0
-            let pageRect = CGRect(x: 0, y: 0, width: pageWidth, height: pageHeigth)
-            
-            let renderer = UIGraphicsPDFRenderer(bounds: pageRect, format: format)
-            
-            let data = renderer.pdfData { (context) in
-                context.beginPage()
-                let atributes = [
-                    NSAttributedString.Key.font: UIFont.boldSystemFont(ofSize: 72)
-                ]
-            let text = "PDF de prueba"
-                text.draw(at:CGPoint(x:0,y:0),withAttributes: atributes)
-            }
-            
-            return data
+    }//EndfuncGenerateButtonTapped
+    
+    @IBAction func selectImageTouched(_ sender: Any) {
+      let actionSheet = UIAlertController(title: "Select Photo", message: "Where do you want to select a photo?", preferredStyle: .actionSheet)
+      
+      let photoAction = UIAlertAction(title: "Photos", style: .default) { (action) in
+        if UIImagePickerController.isSourceTypeAvailable(.savedPhotosAlbum) {
+          let photoPicker = UIImagePickerController()
+          photoPicker.delegate = self
+          photoPicker.sourceType = .photoLibrary
+          photoPicker.allowsEditing = false
+          
+          self.present(photoPicker, animated: true, completion: nil)
         }
+      }
+      actionSheet.addAction(photoAction)
+      
+      let cameraAction = UIAlertAction(title: "Camera", style: .default) { (action) in
+        if UIImagePickerController.isSourceTypeAvailable(.camera) {
+          let cameraPicker = UIImagePickerController()
+          cameraPicker.delegate = self
+          cameraPicker.sourceType = .camera
+          
+          self.present(cameraPicker, animated: true, completion: nil)
+        }
+      }
+      actionSheet.addAction(cameraAction)
+      
+      let cancelAction = UIAlertAction(title: "Cancel", style: .cancel, handler: nil)
+      actionSheet.addAction(cancelAction)
+      
+      self.present(actionSheet, animated: true, completion: nil)
     }
     
+   
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+      if segue.identifier == "previewSegue" {
+        guard let vc = segue.destination as? pdfPreviewViewController else { return }
+        
+        if let title = nameTextEntry.text, let body = bodyTextView.text,
+          let image = imagePreview.image, let model = modelTextField.text {
+          let pdfCreator = PDFCreator(title: title, body: body,
+                                      image: image, model: model)
+          vc.documentData = pdfCreator.createPDF()
+        }
+      }
+    }
+    
+
 }
+
+
+extension DetailsViewController: UIImagePickerControllerDelegate,UINavigationControllerDelegate {
+  func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey: Any]) {
+    
+    guard let selectedImage = info[UIImagePickerController.InfoKey.originalImage] as? UIImage else {
+      return
+    }
+    
+    imagePreview.image = selectedImage
+    imagePreview.isHidden = false
+    
+    dismiss(animated: true, completion: nil)
+  }
+}
+
+
